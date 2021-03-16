c9703209b518fc802674f87726bf1db33a378305
Manuelhrv99 <74119725+Manuelhrv99@users.noreply.github.com>
2021-03-16 16:50:06 -0600

    Create main.m

---
+++ main.m
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+
+#import "Calificaciones.h"
+
+int main (int argc, const char * argv[])
+{
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        
+        //char Mensaje = "";
+        
+        NSLog(@"hola felipe");
+        NSLog(@"%@",[Mensaje Saludo2]);
+        NSLog(@"%@",[Mensaje saludonombre:@"felipe" Andapellido:@"Hernandez"]);
+        [pool drain];
+        return 0;
+}
